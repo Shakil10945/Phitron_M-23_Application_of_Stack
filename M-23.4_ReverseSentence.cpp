462513925b8a2c633721f0cb2f292b9c9084326b
Shakil Khan <67122643+Shakil10945@users.noreply.github.com>
2023-03-25 15:44:25 +0600

    Update M-23.4_ReverseSentence.cpp

--- M-23.4_ReverseSentence.cpp
+++ M-23.4_ReverseSentence.cpp
@@ -1,34 +1,81 @@
 #include<bits/stdc++.h>
 #include"TMyStack.h"
-
 using namespace std;
 
-void reverseSentence(string chk)
+int globalID = 100;
+
+class Person
 {
-    Stack <string> st;
-
-    for(int i=0;i<chk.length();i++)
-    {
-        string word = "";
-
-        while(i<chk.length() && chk[i] != ' ')
+    string name;
+    int id;
+    float salary;
+    
+    public:
+        Person()
         {
-            word+=chk[i];
-            cout<< chk[i]<<endl;
-            i++;
+            name = "";
+            id = -1;
+            salary = -1.0;
         }
-        st.push(word);
-    }
+        
+        void setName(string name)
+        {
+            this->name = name;
+        }
+        void setSalary(float salary)
+        {
+            this->salary=salary;
+        }
 
-    while(!st.empty())
-    {
-        cout<< st.Top()<<" ";
-        st.pop();
-    }
-}
+        Person(string name, float salary)
+        {
+            setName(name);
+            setSalary(salary);
+            id=globalID;
+            globalID++;
+        }
+
+        int getID()
+        {
+            return id;
+        }
+        string getName()
+        {
+            return name;
+        }
+        float getSalary()
+        {
+            return salary;
+        }
+
+        void print()
+        {
+            cout<<name<<" | "<< id << " | "<<salary<<endl;
+        }
+};
+
 int main()
 {
-    string S = "I am Shakil Khan";
-    reverseSentence(S);
+    Stack <Person> st;
+    Person a("Shakil Khan", 8000);
+    Person b("Rifat Hasan", 9000);
+    Person c("Hasibul Hasan", 9500);
+
+    st.push(a);
+    st.push(b);
+    st.push(c);
+
+ /*   while(!st.empty())
+    {
+        Person printObj;
+        printObj = st.pop();
+        printObj.print();
+    }
+    */
+   Person printObj;
+   printObj = st.Top();
+   printObj.print();
+   cout<< st.size()<<endl;
+   
     return 0;
 }
