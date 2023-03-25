45e835115748fab5fb673fbf7c7c4a3e04f7152b
Shakil Khan <67122643+Shakil10945@users.noreply.github.com>
2023-03-25 14:14:21 +0600

    Salary

---
+++ M-23_3_Salary.cpp
@@ -0,0 +1,75 @@
+#include<bits/stdc++.h>
+#include"TMyStack.h"
+using namespace std;
+
+int globalID = 100;
+
+class Person
+{
+    string name;
+    int id;
+    float salary;
+    
+    public:
+        Person()
+        {
+            name = "";
+            id = -1;
+            salary = -1.0;
+        }
+        
+        void setName(string name)
+        {
+            this->name = name;
+        }
+        void setSalary(float salary)
+        {
+            this->salary=salary;
+        }
+
+        Person(string name, float salary)
+        {
+            setName(name);
+            setSalary(salary);
+            id=globalID;
+            globalID++;
+        }
+
+        int getID()
+        {
+            return id;
+        }
+        string getName()
+        {
+            return name;
+        }
+        float getSalary()
+        {
+            return salary;
+        }
+
+        void print()
+        {
+            cout<<name<<" | "<< id << " | "<<salary<<endl;
+        }
+};
+
+int main()
+{
+    Stack <Person> st;
+    Person a("Shakil Khan", 8000);
+    Person b("Rifat Hasan", 9000);
+    Person c("Hasibul Hasan", 9500);
+
+    st.push(a);
+    st.push(b);
+    st.push(c);
+
+    while(!st.empty())
+    {
+        Person X;
+        X = st.pop();
+        X.print();
+    }
+    return 0;
+}
